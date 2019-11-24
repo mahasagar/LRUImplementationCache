f5290cc131eca6ddfc3b2e839f099b1234f82e3f
Sagar Chaudhari <chaudharisagard@gmail.com>
2019-11-24 21:09:54 +0530

    DELETE,EXPORT, IMPORT Added.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const LRU = new NodeLRU();
 
 
 LRU.setup('file',{
-    path : '/home/medibox',
-    fileName : 'sagar',
+    // path : '/home/medibox',
+    // fileName : 'sagar',
     count : 50
 });
 
@@ -12,9 +12,19 @@ LRU.set('one','sagar');
 LRU.set('two','arif');
 LRU.set('three','kunal');
 LRU.set('one','rihana');
-let data = LRU.get('one');
-console.log('one : ',data);
+let oneData = LRU.get('one');
+console.log('one : ',oneData);
 
+console.log('\nDelete data at two key')
+LRU.delete('two'); 
+var twoData = LRU.get('two');
+console.log('two : ',twoData);
+var threeData = LRU.get('three');
+console.log('three : ',threeData);
+
+let res = LRU.import('/home/medibox/Desktop/importFile.csv');
+
+LRU.export('/home/medibox/Desktop/exportFile.csv');
 
 
 
@@ -27,6 +37,12 @@ console.log('one : ',data);
 //     count : 50
 // });
 
-// LRU.set('one','sagar');
-// LRU.set('two','sagar');
+// LRU.set('one11','sagar');
+// LRU.set('one12','sagar12');
+// LRU.set('one13','sagar13');
+// LRU.set('one12','gayatri');
+
+// var data1 = LRU.get('one12');
+// console.log('\n one12 : ',data1);
+
 
